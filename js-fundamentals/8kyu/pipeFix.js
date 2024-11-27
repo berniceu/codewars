6b36670c918d08b92b620b143822803ff154c18d
uwituzeb <b.uwituze@alustudent.com>
2024-11-27 10:47:57 +0200

    8kyu js fundamentals codewars

---
+++ js-fundamentals/8kyu/pipeFix.js
@@ -0,0 +1,10 @@
+function pipeFix(numbers){
+    const minNum  = Math.min(...numbers)
+    const maxNum = Math.max(...numbers)
+    const arr = []
+    for(let i = minNum; i <= maxNum; i++){
+      arr.push(i)
+    }
+    
+    return arr
+  }
